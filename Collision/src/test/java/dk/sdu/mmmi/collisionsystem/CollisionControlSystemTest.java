899462a2cfe42c972f67d87579f71d5117736183
Oliver Larsen <Oliver1703dk@hotmail.com>
2024-05-19 20:15:57 +0200

    Smol changes

--- Collision/src/test/java/dk/sdu/mmmi/collisionsystem/CollisionControlSystemTest.java
+++ Collision/src/test/java/dk/sdu/mmmi/collisionsystem/CollisionControlSystemTest.java
@@ -1,7 +1,10 @@
 package dk.sdu.mmmi.collisionsystem;
 
+
 import dk.sdu.mmmi.cbse.common.data.Entity;
 
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,19 +13,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CollisionControlSystemTest {
 
+
+    private GameData gameData;
+    private World world;
+    private CollisionControlSystem collisionControlSystem;
+
     @BeforeEach
     void setUp() {
+        gameData = new GameData();
+        world = new World();
+        collisionControlSystem = new CollisionControlSystem();
     }
 
 
     @AfterEach
     void tearDown() {
+        gameData = null;
+        world = null;
+        collisionControlSystem = null;
     }
 
 
     @Test
     void isCollision() {
-
         Entity asteroid = new Entity();
         Entity bullet = new Entity();
         asteroid.setPolygonCoordinates(20, 0, 14, 14, 0, 20, -14, 14, -20, 0, -14, -14, 0, -20, 14, -14);
@@ -30,16 +43,13 @@ class CollisionControlSystemTest {
 
         asteroid.setX(10);
         asteroid.setY(10);
-        bullet.setX(15);
-        bullet.setY(15);
-
-        CollisionControlSystem collisionControlSystem = new CollisionControlSystem();
+        bullet.setX(11);
+        bullet.setY(11);
 
         assertTrue(collisionControlSystem.isCollision(asteroid, bullet));
     }
     @Test
     void isNotCollision() {
-
         Entity asteroid = new Entity();
         Entity bullet = new Entity();
         asteroid.setPolygonCoordinates(20, 0, 14, 14, 0, 20, -14, 14, -20, 0, -14, -14, 0, -20, 14, -14);
@@ -50,10 +60,9 @@ class CollisionControlSystemTest {
         bullet.setX(150);
         bullet.setY(150);
 
-        CollisionControlSystem collisionControlSystem = new CollisionControlSystem();
-
         assertFalse(collisionControlSystem.isCollision(asteroid, bullet));
     }
+
 
 
 
