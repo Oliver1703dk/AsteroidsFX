8c22f123c66a4a03cda2e5d6bb81a8462e43e209
Oliver Larsen <Oliver1703dk@hotmail.com>
2024-05-20 09:55:59 +0200

    Nothing

--- Asteroid/src/main/java/module-info.java
+++ Asteroid/src/main/java/module-info.java
@@ -4,7 +4,6 @@ import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 module Asteroid {
-    exports dk.sdu.mmmi.asteroidsystem;
     requires Common;
     provides IGamePluginService with AsteroidPlugin;
     provides IEntityProcessingService with AsteroidControlSystem;
